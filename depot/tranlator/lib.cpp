d941fa3f77695201160b80e157014d687d9dd0d7
Toshiki Kataoka <tos@knit.local>
2014-07-26 05:36:13 +0900

    a value of P might be just an integer

--- depot/tranlator/lib.cpp
+++ depot/tranlator/lib.cpp
@@ -5,130 +5,128 @@
 
 #include <glog/logging.h>
 
-struct Var;
+struct P;
 
-//std::string Var::to_string();
+struct Pair {
+  std::shared_ptr<P> car, cdr;
 
-struct P {
-  std::shared_ptr<Var> car, cdr;
-
-  P(){
+  Pair(){
     car = NULL; cdr = NULL;
   }
 
-  P(int a, int d);
-  P(int a, P d);
-  P(P a, int d);
-  P(P a, P d);
+  //Pair(int a, int d);
+  //Pair(int a, P d);
+  //Pair(P a, int d);
+  Pair(P a, P d);
 
   std::string to_string();
 };
 
-enum var_type {TYPE_INT, TYPE_P};
+enum var_type {TYPE_UNDEFINED, TYPE_INT, TYPE_PAIR};
 
-struct Var {
+struct P {
   enum var_type type;
   //union {
     int data_int;
-    P data_P;
+    Pair data_Pair;
   //};
 
-  Var (const Var& v) {
+  P (const P& v) {
     type = v.type;
     switch (type) {
+      case TYPE_UNDEFINED:
+	break;
       case TYPE_INT:
 	data_int = v.data_int;
 	break;
-      case TYPE_P:
-	data_P = v.data_P;
+      case TYPE_PAIR:
+	data_Pair = v.data_Pair;
 	break;
       default:
-	LOG(FATAL) << "Var: undefined type";
+	LOG(FATAL) << "P: undefined type";
     }
   }
 
-  Var (int n) {
+  P () {  // debug
+    type = TYPE_UNDEFINED;
+  }
+
+  P (int n) {
     type = TYPE_INT;
     data_int = n;
   }
 
-  Var (P p) {
-    type = TYPE_P;
-    data_P = p;
+  P (Pair p) {
+    type = TYPE_PAIR;
+    data_Pair = p;
+  }
+
+  P (int a, int d) {
+    *this = P(Pair(P(a),P(d)));
+  }
+
+  P (int a, P d) {
+    *this = P(Pair(P(a),d));
+  }
+
+  P (P a, int d) {
+    *this = P(Pair(a,P(d)));
+  }
+
+  P (P a, P d) {
+    *this = P(Pair(a,d));
+  }
+
+  int toInt() {
+    CHECK(type == TYPE_INT);
+    return data_int;
+  }
+
+  Pair toPair() {
+    CHECK(type == TYPE_PAIR);
+    return data_Pair;
   }
 
   std::string to_string();
 };
 
-P pair_var(Var a, Var d) {
-  P ret;
-  std::shared_ptr<Var> car(new Var(a));
-  std::shared_ptr<Var> cdr(new Var(d));
-  ret.car = car;
-  ret.cdr = cdr;
-  return ret;
+Pair::Pair(P a, P d) {
+  std::shared_ptr<P> car(new P(a));
+  std::shared_ptr<P> cdr(new P(d));
+  this->car = car;
+  this->cdr = cdr;
 }
 
-P::P(int a, int d) {
-  *this = pair_var(Var(a), Var(d));
+int firstInt(P v) {
+  return v.toPair().car->toInt();
 }
 
-P::P(int a, P d) {
-  *this = pair_var(Var(a), Var(d));
+P firstP(P v) {
+  return *v.toPair().car;
 }
 
-P::P(P a, int d) {
-  *this = pair_var(Var(a), Var(d));
+int secondInt(P v) {
+  return v.toPair().cdr->toInt();
 }
 
-P::P(P a, P d) {
-  *this = pair_var(Var(a), Var(d));
-}
-
-//P P(int a, int d) {
-//  return pair_var(Var(a), Var(d));
-//}
-
-int var2int (Var v) {
-  CHECK(v.type == TYPE_INT);
-  return v.data_int;
-}
-
-struct P var2P (Var v) {
-  CHECK(v.type == TYPE_P);
-  return v.data_P;
-}
-
-int firstInt(P p) {
-  return var2int(*p.car);
-}
-
-P firstP(P p) {
-  return var2P(*p.car);
-}
-
-int secondInt(P p) {
-  return var2int(*p.cdr);
-}
-
-P secondP(P p) {
-  return var2P(*p.cdr);
-}
-
-
-
-std::string Var::to_string() {
-  switch (type) {
-    case TYPE_INT:
-      return std::to_string(data_int);
-    case TYPE_P:
-      return data_P.to_string();
-    default:
-      LOG(FATAL) << "Var: undefined type";
-  }
+P secondP(P v) {
+  return *v.toPair().cdr;
 }
 
 std::string P::to_string() {
+  switch (type) {
+    case TYPE_UNDEFINED:
+      return "(undefined)";
+    case TYPE_INT:
+      return std::to_string(data_int);
+    case TYPE_PAIR:
+      return data_Pair.to_string();
+    default:
+      LOG(FATAL) << "P: undefined type";
+  }
+}
+
+std::string Pair::to_string() {
   std::string ret;
   ret += "<";
   ret += car->to_string();
