0add0b4a9ae8f30b4a0ad655ed963e2308cf691e
sulume <sulume@sulume-mpb13p.local>
2014-07-27 03:26:21 +0900

    Revert "lman response safety"
    This reverts commit 4b54bdbdeadcf086e6241046a0f344b08aa7b688.

--- depot/sim/sim.cc
+++ depot/sim/sim.cc
@@ -148,9 +148,8 @@ int Game::Start() {
     // *** 1. moves
     if (tick_ == utc_lman_next_move) {
       int d = lman_[0]->Step();
-      if (0 <= d && d < 4) {
-        LOG(WARNING) << "Lambda-Man responded wrong direction " << d;
-      } else if (lman_[0]->CanMove(*this, d)) {
+      CHECK(0 <= d && d < 4) << d;
+      if (lman_[0]->CanMove(*this, d)) {
         lman_[0]->SetDirection(d);
         CHECK(lman_[0]->Move());
       } else {
