17ed6be6eb22251f4b3e9af8e8c47f567b8df4f3
sulume <sulume@sulume-mpb13p.local>
2014-07-26 14:31:03 +0900

    FakeLambdaMan

--- depot/sim/sim_main.cc
+++ depot/sim/sim_main.cc
@@ -8,13 +8,21 @@
 
 DEFINE_string(maze, "example_maze.txt", "Text file that contains maze map");
 
+class FakeLambdaMan : public LambdaMan {
+public:
+  void Init() override {}
+  int Step() override {
+    return rand() % 4;
+  }
+};
+
 int main(int argc, char** argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
 
   Game game;
   game.AddGhostFactory(new ghost::FickleGhostFactory);
-  game.SetLambdaMan(nullptr);
+  game.SetLambdaMan(new FakeLambdaMan);
   std::ifstream mazefile(FLAGS_maze);
   game.ParseMaze(mazefile);
 
