18585a96bdea1c87377e8b67c808d40c02e04999
Toshiki Kataoka <tos@knit.local>
2014-07-26 18:51:17 +0900

    tree (test passed)

--- depot/sbl/test/tree_test.cpp
+++ depot/sbl/test/tree_test.cpp
@@ -26,9 +26,28 @@ void debug_tree2d(){
     P(P(5, P(3, P(5, P(8, P(9, P(7, 0)))))),
     P(P(2, P(3, P(8, P(4, P(6, P(2, 0)))))),
     0)));
-  tree = list2d_to_tree2d(list2d);
+  tree = list2d_to_tree2d(list);
   debug(tree);
-  tree = write_tree2d(tree);
+  tree = write_tree2d(tree,1,4,12345);
+  debug(tree);
+  debug(read_tree2d(tree,0,0));
+  debug(read_tree2d(tree,0,1));
+  debug(read_tree2d(tree,0,2));
+  debug(read_tree2d(tree,0,3));
+  debug(read_tree2d(tree,0,4));
+  debug(read_tree2d(tree,0,5));
+  debug(read_tree2d(tree,1,0));
+  debug(read_tree2d(tree,1,1));
+  debug(read_tree2d(tree,1,2));
+  debug(read_tree2d(tree,1,3));
+  debug(read_tree2d(tree,1,4));
+  debug(read_tree2d(tree,1,5));
+  debug(read_tree2d(tree,2,0));
+  debug(read_tree2d(tree,2,1));
+  debug(read_tree2d(tree,2,2));
+  debug(read_tree2d(tree,2,3));
+  debug(read_tree2d(tree,2,4));
+  debug(read_tree2d(tree,2,5));
 }
 
 void run() {
