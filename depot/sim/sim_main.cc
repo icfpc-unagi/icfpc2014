10d3463e987675a2e78f892dc245cfb8061d0057
imos <github@imoz.jp>
2014-07-27 07:04:11 +0900

    Add a flag to suppress messages.

--- depot/sim/sim_main.cc
+++ depot/sim/sim_main.cc
@@ -8,13 +8,18 @@
 #include "ghost/ai/fickle.h"
 
 DEFINE_string(maze, "depot/maze/world-classic.txt", "Text file that contains maze map");
+DEFINE_bool(silent, false, "Suppress status messages.");
 DECLARE_bool(logtostderr);
 
 int main(int argc, char** argv) {
   google::ParseCommandLineFlags(&argc, &argv, true);
   google::InitGoogleLogging(argv[0]);
-  FLAGS_logtostderr = true;
-  
+  if (FLAGS_silent) {
+    FLAGS_logtostderr = false;
+  } else {
+    FLAGS_logtostderr = true;
+  }
+
   if (FLAGS_maze.empty()) {
     std::cerr << "Usage: simulate_foobar --maze=<filename> --ghosts=<ghost>[,<ghost>]" << std::endl;
     return 1;
