5bb7dea1c62999dc242bac612c9ae02d6d424e77
imos <github@imoz.jp>
2014-07-26 11:29:52 +0900

    Make sim_main use gflags and glog.

--- depot/sim/sim_main.cc
+++ depot/sim/sim_main.cc
@@ -1,11 +1,16 @@
-#include "sim/sim.h"
 #include <fstream>
+
 #include <gflags/gflags.h>
 #include <glog/logging.h>
 
+#include "sim/sim.h"
+
 DEFINE_string(maze, "example_maze.txt", "Text file that contains maze map");
 
-int main() {
+int main(int argc, char** argv) {
+  google::ParseCommandLineFlags(&argc, &argv, true);
+  google::InitGoogleLogging(argv[0]);
+
   Game game;
   game.AddGhostFactory(nullptr);
   game.SetLambdaMan(nullptr);
