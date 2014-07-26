cd8dc914d60aa1b5322766c21acf7a6b30c2d05d
Toshiki Kataoka <tos@knit.local>
2014-07-26 19:05:24 +0900

    replicate_list, length_list

--- depot/sbl/list.cpp
+++ depot/sbl/list.cpp
@@ -2,8 +2,13 @@
 
 // P read_list(P list, int n)
 // P write_list(P list, int n, P value)
-// P read_list(P list, int i, int j)
-// P write_list(P list, int i, int j, P value) {
+//
+// list2d
+// P read_list2d(P list, int i, int j)
+// P write_list2d(P list, int i, int j, P value) {
+//
+// int length_list(P list)
+// P reverse_list(P list)
 #pragma once
 
 P read_list(P list, int n) {
@@ -38,12 +43,12 @@ P write_list2d(P list, int i, int j, P val) {
   return write_list(list, i, write_list(read_list(list,i), j, val) );
 }
 
-int length(P list) {
+int length_list(P list) {
   int res;
   if (atom(list)) {
     res = 0;
   } else {
-    res = 1 + length(snd(list));
+    res = 1 + length_list(snd(list));
   }
   return res;
 }
@@ -62,3 +67,13 @@ P reverse_list(P list) {
   return reverse_list1(list, top(0));
 }
 
+
+P replicate_list(int n, P val) {
+  P res;
+  res = top(0);
+  while (n > 0) {
+    res = P(val, res);
+    n = n-1;
+  }
+  return res;
+}
--- depot/sbl/test/list_test.cpp
+++ depot/sbl/test/list_test.cpp
@@ -7,9 +7,10 @@ void debug_list(){
   P list;
   list = P(3, P(1, P(4, P(1, P(5, P(9, 0))))));
   debug(list);
-  debug(length(list));
+  debug(length_list(list));
   list = reverse_list(list);
   debug(list);
+  debug(replicate_list(3,-1));
 }
 
 void run() {
