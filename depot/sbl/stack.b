ff7aafbbb3e9297585afc21633eb414f27c52b3f
Takuya Akiba <t.akiba.65536@gmail.com>
2014-07-26 17:49:28 +0900

    すたっく

---
+++ depot/sbl/stack.b
@@ -0,0 +1,19 @@
+P create_stack() {
+  return top(0);
+}
+
+int is_empty_stack(P stack) {
+  return atom(stack);
+}
+
+P push_stack(P stack, P x) {
+  return P(x, stack);
+}
+
+P top_stack(P stack) {
+  return fst(stack);
+}
+
+P pop_stack(P stack) {
+  return snd(stack);
+}
---
+++ depot/sbl/stack_test.cc
@@ -0,0 +1,16 @@
+#include "translator/lib.h"
+#include "stack.b"
+
+void run() {
+  P s;
+  s = create_stack();
+  s = push_stack(s, top(1));
+  s = push_stack(s, top(2));
+  s = push_stack(s, top(3));
+  s = push_stack(s, top(4));
+
+  while (is_empty_stack(s) == 0) {
+    debug(top_stack(s));
+    s = pop_stack(s);
+  }
+}
