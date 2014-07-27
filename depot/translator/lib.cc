9ba656a6168f786bd39cee3b0b69477aa14d8d4f
sulume <sulume@sulume-mpb13p.local>
2014-07-28 04:02:54 +0900

    Revert "more readable logging for color printing"
    This reverts commit 3931101b2366a5c4a31341c43e176454bd98934c.

--- depot/translator/lib.cc
+++ depot/translator/lib.cc
@@ -66,7 +66,9 @@ void debug(const P& v) {
     if (FLAGS_print_for_test) {
       printf("trace lambdaman: %s\n", v.to_string().c_str());
     }
-    std::cerr << "Lambda-Man: " << v.to_string() << (FLAGS_print_color ? "" : CLEARLINEAFTER) << std::endl;
+    std::cerr << "Lambda-Man: " << v.to_string();
+    if (FLAGS_print_color) std::cerr << "    ";
+    std::cerr << std::endl;
   }
   return;
 }
