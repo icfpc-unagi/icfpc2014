4c52d2c48727b6a84cd22d1e6fe59f0218b3cde1
Takuya Akiba <t.akiba.65536@gmail.com>
2014-07-27 16:02:01 +0900

    array256 をとりあえず置いてみる

---
+++ depot/sbl/array256.cpp
@@ -0,0 +1,34 @@
+P list_to_array256_rec(P t, int size) {
+  P tmp;
+  if (atom(t)) {
+    t = P(0, 0);
+  } else {
+    if (size > 1) {
+      t = list_to_array256_rec(t, size / 2);
+      tmp = list_to_array256_rec(snd(t), size / 2);
+      t = P(P(fst(t), fst(tmp)), snd(tmp));
+    }
+  }
+  return t;
+}
+
+P list_to_array256(P list) {
+  return fst(list_to_array256_rec(list, 256));
+}
+
+P set_array256_rec(P t, int i, P v, int n) {
+  if (n == 0) {
+    t = v;
+  } else {
+    if (i < n) {
+      t = P(set_array256_rec(fst(t), i, v, n / 2), snd(t));
+    } else {
+      t = P(fst(t), set_array256_rec(snd(t), i - n, v, n / 2));
+    }
+  }
+  return t;
+}
+
+P set_array256(P t, int i, P v) {
+  return set_array256_rec(t, i, v, 128);
+}
---
+++ depot/sbl/array256.h
@@ -0,0 +1,3 @@
+P list_to_array256(P list);
+P get_array256(P t, int i);
+P set_array256(P t, int i, P v);
