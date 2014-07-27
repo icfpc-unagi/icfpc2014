c2602bba2116599ccf4b9eec646d39e8645a4144
sulume <sulume@sulume-mpb13p.local>
2014-07-27 20:50:16 +0900

    reduce ghosts

--- depot/maze/gen-test.sh
+++ depot/maze/gen-test.sh
@@ -12,8 +12,8 @@ ${GEN} -size=50  -ghosts=6  -powerpills=6  -maker=digger > depot/maze/test-50.tx
 ${GEN} -size=70  -ghosts=8  -powerpills=7  -maker=digger > depot/maze/test-70.txt
 ${GEN} -size=100 -ghosts=10 -powerpills=7  -maker=digger > depot/maze/test-100.txt
 ${GEN} -size=150 -ghosts=15 -powerpills=8  -maker=digger > depot/maze/test-150.txt
-${GEN} -size=200 -ghosts=25 -powerpills=9  -maker=digger > depot/maze/test-200.txt
-${GEN} -size=256 -ghosts=40 -powerpills=10 -maker=digger > depot/maze/test-256.txt
+${GEN} -size=200 -ghosts=20 -powerpills=9  -maker=digger > depot/maze/test-200.txt
+${GEN} -size=256 -ghosts=30 -powerpills=10 -maker=digger > depot/maze/test-256.txt
 
 ${GEN} -size=22  -ghosts=4  -powerpills=4  -maker=grid > depot/maze/test2-22.txt
 ${GEN} -size=32  -ghosts=5  -powerpills=5  -maker=grid > depot/maze/test2-32.txt
@@ -22,15 +22,15 @@ ${GEN} -size=50  -ghosts=6  -powerpills=6  -maker=grid > depot/maze/test2-50.txt
 ${GEN} -size=70  -ghosts=8  -powerpills=7  -maker=grid > depot/maze/test2-70.txt
 ${GEN} -size=100 -ghosts=10 -powerpills=7  -maker=grid > depot/maze/test2-100.txt
 ${GEN} -size=150 -ghosts=15 -powerpills=8  -maker=grid > depot/maze/test2-150.txt
-${GEN} -size=200 -ghosts=25 -powerpills=9  -maker=grid > depot/maze/test2-200.txt
-${GEN} -size=256 -ghosts=40 -powerpills=10 -maker=grid > depot/maze/test2-256.txt
+${GEN} -size=200 -ghosts=20 -powerpills=9  -maker=grid > depot/maze/test2-200.txt
+${GEN} -size=256 -ghosts=30 -powerpills=10 -maker=grid > depot/maze/test2-256.txt
 
 ${GEN} -width=40 -height=20 -ghosts=5  -powerpills=3  -maker=digger > depot/maze/rect-40x20.txt
-${GEN} -width=80 -height=20 -ghosts=8  -powerpills=6  -maker=digger > depot/maze/rect-80x20.txt
-${GEN} -width=80 -height=20 -ghosts=10  -powerpills=7  -maker=digger > depot/maze/rect-160x20.txt
+${GEN} -width=80 -height=20 -ghosts=6  -powerpills=6  -maker=digger > depot/maze/rect-80x20.txt
+${GEN} -width=80 -height=20 -ghosts=8  -powerpills=7  -maker=digger > depot/maze/rect-160x20.txt
 ${GEN} -width=20 -height=40 -ghosts=5  -powerpills=3  -maker=digger > depot/maze/rect-20x40.txt
-${GEN} -width=20 -height=80 -ghosts=8  -powerpills=6  -maker=digger > depot/maze/rect-20x80.txt
-${GEN} -width=20 -height=80 -ghosts=10  -powerpills=7  -maker=digger > depot/maze/rect-20x160.txt
+${GEN} -width=20 -height=80 -ghosts=6  -powerpills=6  -maker=digger > depot/maze/rect-20x80.txt
+${GEN} -width=20 -height=80 -ghosts=8 -powerpills=7  -maker=digger > depot/maze/rect-20x160.txt
 
 if [ -d /dropbox ]; then
   cp -f depot/maze/*.txt /dropbox/maze/
