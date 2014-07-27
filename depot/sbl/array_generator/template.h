a22e6b9b3fce4b682b275a025ec6618011c53e50
Takuya Akiba <t.akiba.65536@gmail.com>
2014-07-27 16:15:07 +0900

    インクルードガードも生成

--- depot/sbl/array_generator/template.h
+++ depot/sbl/array_generator/template.h
@@ -1,3 +1,8 @@
+#ifndef SBL_ARRAY<%=n%>_H_
+#define SBL_ARRAY<%=n%>_H_
+
 P list_to_array<%=n%>(P list);
 P get_array<%=n%>(P t, int i);
 P set_array<%=n%>(P t, int i, P v);
+
+#endif  // SBL_ARRAY<%=n%>_H_
